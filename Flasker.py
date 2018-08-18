b6d196f483d5bbccd46740b7d0536e3d64b59ab8
daniel <danielhhowell@aol.com>
2018-08-18 20:03:35 +0200

    -

--- Flasker.py
+++ Flasker.py
@@ -1,14 +1,12 @@
 from flask import Flask, render_template, flash, request
 from wtforms import Form, StringField, validators
 import Main
-import time
 
 
 app = Flask(__name__)
 
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = 'e5ac358c-f0bf-11e5-9e39-d3b532c10a28'
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 class InputForm(Form):
     search = StringField('search:', [validators.required()])
@@ -22,7 +20,6 @@ def index():
 
         if form.validate():
             Main.main_scraper(search)
-            time.sleep(.3)
             return render_template('/map.html')
 
         else:
@@ -31,4 +28,4 @@ def index():
     return render_template('index.html', form=form)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port='80')
+    app.run(threaded=True)
